365d512e94bb508446db041717a9902512e1b4b8
yeswanth <yeswanthreddy2801@gmail.com>
2026-05-17 17:10:34 +0530

    added education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,4 +1,7 @@
 import './Education.css';
+import Location from '../assets/location.webp'
+import Calender from '../assets/calender.webp'
+import CapPic from '../assets/educationCap.webp'
 export default function Education()
 {
     return(
@@ -6,6 +9,7 @@ export default function Education()
             <h1 className='heading'>Education</h1>
             <div className='container'>
                 <EducationContainer />
+                {/* <EducationContainer /> */}
             </div>
         </>
     );
@@ -15,7 +19,49 @@ function EducationContainer()
     return(
         <>
         <div className='educationContainer'>
-
+        <div className='headOfEducation'>
+            <img src={CapPic} alt="" className='logo' />
+            <div>
+                <h2>Education</h2>
+                <p>My Academic Journey</p>
+            </div>
+        </div>
+        <div className='college'>
+            <p><span className="span">B.Tech Information Technology</span> <br />
+Anil Neerukonda Institute Of Technology & Sciences</p>
+<div className='cgpa'>
+        <div className='datePlace'>
+            <div className='calLoc'>
+                <img src={Calender} alt="" className="logosss"/>
+                <p>2023-2027</p>
+            </div>
+            <div className='calLoc'>
+                <img src={Location} alt="" className="logosss"/>
+                <p>Visakhapatnam</p>
+            </div>
+        </div>
+        <p className='cgpaInside'><span className='span'>9.3</span>/10 CGPA</p>
+</div>
+            
+        </div>
+        <div className='college'>
+            <p><span className="span">Senior Secondory (XII) ,Science</span> <br />
+Sree Basara Junior College</p>
+<div className='cgpa'>
+        <div className='datePlace'>
+            <div className='calLoc'>
+                <img src={Calender} alt="" className="logosss"/>
+                <p>2021-2023</p>
+            </div>
+            <div className='calLoc'>
+                <img src={Location} alt="" className="logosss"/>
+                <p>Visakhapatnam</p>
+            </div>
+        </div>
+        <p className='cgpaInside'><span className='span'>951</span>/1000 marks</p>
+</div>
+            
+        </div>
         </div>
         </>
     );
